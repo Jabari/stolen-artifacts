143ae30ac16c3ccc9f532d8319ccaf7984e23041
Jabari <jabaricmartin@gmail.com>
2017-12-26 15:40:28 -0500

    2 buttons on landing page

--- components/FrontPage.vue
+++ components/FrontPage.vue
@@ -1,12 +1,18 @@
 <template>
 	<div id="landing-page-experience">
 		<div class="above-the-fold record-gif">
-			<div id="logo--spinning" :class="welcomeAnimation === true ? 'user-welcome-sequence' : ''">
-				a
-			</div>
+			<button id="logo--spinning" class="logo" :class="welcomeAnimation === true ? 'user-welcome-sequence' : ''">
+				<div class="sa">
+					a
+				</div>
+			</button>
+			<button id="nutt--spinning" class="logo" :class="welcomeAnimation === true ? 'user-welcome-sequence' : ''">
+				<!-- <img src='data:image/svg+xml;utf8,<svg viewBox="0 0 100 100"><use xlink:href="./static/walnut.svg#walnut"></use></svg>'> -->
+				<img class="nutt" src="../static/walnut.svg">
+			</button>
 		</div>
-		
 	</div>
+
 </template>
 <script type="text/javascript">
 export default {
@@ -23,10 +29,10 @@ export default {
 		var that = this;
 
 		setTimeout(function() {
-			that.welcomeAnimation = true;
+			//that.welcomeAnimation = true;
 		})
 		setTimeout(function() {
-			that.$router.push('/sapiens')
+			//that.$router.push('/sapiens')
 		}, 10000)
 		
 		// method that listens for drag-left/swipe-left event and spins the ~ 
@@ -46,22 +52,39 @@ $welcome-duration: 6s;
 
 	.above-the-fold {
 		background: #010101;
+		display: flex;
+		flex-flow: row wrap;
 		&.record-gif {
 			background-image: url('https://s3-us-west-2.amazonaws.com/stolenartifacts.com/imgs/black-white.gif');
 			background-repeat: no-repeat;
 			background-size: cover;
 			height: 100vh;
 		}
-		#logo--spinning {
+		.logo {
 			background: #fff;
 			color: #000;
 			font-size: 10rem;
     	opacity: 1;
-			height: 100%;
+    	position: relative;
 			perspective-origin: bottom;
+			min-width: 200px;
+			width: 50%;
+		}
+		#logo--spinning {
+			.sa {
+				height: 10rem;
+    		font-size: 11rem;
+				position: absolute;
+				left: 50%;
+				line-height: 7.5rem;
+		    top: 50%;
+		    transform: translate(-50%, -50%);
+			}
 			&:before {
 				content: '~';
 				display: block;
+				margin-top: -10rem;
+    		height: 15rem;
 			}
 			&.user-welcome-sequence {
 				opacity: 0;
@@ -70,6 +93,20 @@ $welcome-duration: 6s;
 					animation: user-welcome-animation 2s linear $welcome-delay infinite;
 					transform-origin: center 18rem;
 				}
+			}
+		}
+		#nutt--spinning {
+
+			.nutt {
+			  height: 10rem;
+		    position: absolute;
+		    top: 50%;
+		    transform: translate(-50%, -50%);
+			}
+		}
+		@media only screen and (max-width: 399px) {
+			.logo {
+				width: 100% !important;
 			}
 		}
 	}
